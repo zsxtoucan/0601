c7b8167031a743258e495f1e1861ebd0509b950e
zsxtoucan <zsxtoucan@gmail.com>
2020-06-01 11:16:10 +0800

    OK

--- AndroidStudioProjects/APP0601/app/src/main/java/com/example/app0601/MainActivity.java
+++ AndroidStudioProjects/APP0601/app/src/main/java/com/example/app0601/MainActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
@@ -38,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void btnREAD(View view) {
+        try{
+            FileInputStream ips =openFileInput(filename);
+            InputStreamReader ipr = new InputStreamReader(ips);
+            char[]buffer =new char[READ_BLOCK_SIZE];
+            String str="";
+            int count;
+            while ((count = ipr.read(buffer))>0) {
+                String s = String.copyValueOf(buffer,0,count);
+                str += s;
+                buffer = new char[READ_BLOCK_SIZE];
+            }
+            ipr.close();
+            Toast.makeText(this,"Read成功了",Toast.LENGTH_LONG).show();
+
+            TextView txvShow = findViewById(R.id.textView2);
+            txvShow.setText("Read內容:\n"+str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
